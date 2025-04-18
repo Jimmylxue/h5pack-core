c95ac1ab483f3a94bb3fb2a3e26e43b7a11e7366
jimmyxuexue <1002661758@qq.com>
2025-04-18 14:10:11 +0800

    更改 start.sh 脚本

--- example/docker/start.sh
+++ example/docker/start.sh
@@ -1,12 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "开始构建 Docker 镜像..."
 docker build -t h5pack-example .
 
+echo "启动 Docker 容器..."
 docker run --name my_container -itd h5pack-example
 
+echo "在容器中执行命令..."
 docker exec -it my_container sh -c "
+    echo '当前工作目录:'
     pwd
+    echo '切换到 /app 目录...'
     cd /app
+    echo '安装 h5pack...'
     yarn add -D h5pack
+    echo '执行 h5pack...'
     npx h5pack
-"
+" 2>&1 | tee docker_output.log
 
+echo "复制 APK 文件..."
 docker cp my_container:/app/app-release.apk ./
+
+echo "清理容器..."
+docker rm -f my_container
+
+echo "完成！"
